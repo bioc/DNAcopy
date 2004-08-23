d8e9866ef003ffe1d9598c62b476e90cbaa88cbf
unknown <unknown@email>
2004-08-23 17:00:07 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_1_4'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_4/madman/Rpacks/DNAcopy@9775 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/rshared.c
+++ src/rshared.c
@@ -1,4 +1,4 @@
 #include <R.h>
 void F77_SUB(rndstart)(void) { GetRNGstate(); }
-void F77_SUB(rndend)(void) { PutRNGstate(); }
+void F77_SUB(rndend)(void) { GetRNGstate(); }
 double F77_SUB(dunif)(void) { return unif_rand(); }
